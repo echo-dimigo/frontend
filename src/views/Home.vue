8d47110d72270f90bccfec4df71f54eb264bb9f5
uhmtoto <uhmtotow@gmail.com>
2019-08-11 19:52:01 +0900

    feat: remove default form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<style lang="scss" scoped>
+</style>
