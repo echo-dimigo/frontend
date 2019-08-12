e4b06308e124b421e4ba6c976bf3a0858e6024d0
uhmtoto <uhmtotow@gmail.com>
2019-08-12 22:53:33 +0900

    feat: initialize echoos components

---
+++ src/echoos/components/EchoosInput.vue
@@ -0,0 +1,43 @@
+<script>
+// todo: emit enter and v-model
+
+export default {
+  name: 'EchoosInput',
+  props: {
+    type: {
+      type: String,
+      default: 'text'
+    },
+    placeholder: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>
+
+<template>
+<input
+  class="input"
+  :type="type"
+  :placeholder="placeholder"
+>
+</template>
+
+<style lang="scss" scoped>
+@import '../../scss/colors.scss';
+
+.input {
+  border: 0;
+  border-radius: 30px;
+
+  background-color: $gray;
+
+  height: 2rem;
+  padding-left: 1rem;
+
+  outline: none;
+
+  font-weight: 200;
+}
+</style>
