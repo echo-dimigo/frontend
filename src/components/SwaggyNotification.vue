69eac748c0b7cdbb0e39752b3b0a857eb3bbdf69
uhmtoto <uhmtotow@gmail.com>
2019-08-14 07:42:34 +0900

    feat: add swaggy notification component

---
+++ src/components/SwaggyNotification.vue
@@ -0,0 +1,133 @@
+<script>
+export default {
+  name: 'SwaggyNotification',
+
+  props: {
+    noties: {
+      type: () => [],
+      default: []
+    }
+  },
+
+  methods: {
+    toggleExpand () {
+      this.expand = !this.expand
+    }
+  },
+
+  data () {
+    return {
+      expand: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="noti">
+    <div class="noti__header">
+      <h3 class="noti__header__title">
+        <i class="noti__icon fas fa-bell" />알림 모음
+        ({{ noties.length }})
+      </h3>
+      <div
+        class="noti__header__expand"
+        @click="toggleExpand"
+      >
+        <i :class="{
+          'noti__header__expand__icon': true,
+          'fas': true,
+          'fa-caret-up': expand,
+          'fa-caret-down': !expand
+        }" />
+      </div>
+    </div>
+    <template v-if="expand">
+      <div class="noti__list">
+        <div
+          :key="`noti-${i}`"
+          v-for="(noti, i) in noties"
+          class="noti__list__item"
+        >
+          <span class="noti__list__item__title">
+            {{ noti.title }}
+          </span>
+          <span class="noti__list__item__content">
+            {{ noti.content }}
+          </span>
+        </div>
+      </div>
+
+      <div class="noti__button">
+        알림 모아보기
+      </div>
+    </template>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import '../scss/colors.scss';
+
+.noti {
+  border-radius: 6px;
+  border: solid 1.5px rgba(21, 19, 19, 0.05);
+
+  padding: 6px 15px;
+
+  /** 임시 */
+  width: 20%;
+  margin: 50px;
+
+  &__icon {
+    margin-right: 8px;
+  }
+
+  &__header {
+    padding: 15px 0;
+
+    &__title {
+      display: inline;
+    }
+
+    &__expand {
+      float: right;
+
+      &__icon {
+        color: $dark-gray;
+        font-size: 1.2rem;
+      }
+    }
+  }
+
+  &__list {
+    border-top: solid 1.5px rgba(21, 19, 19, 0.05);
+    border-bottom: solid 1.5px rgba(21, 19, 19, 0.05);
+
+    &__item {
+      padding: 18px 0;
+
+      &:not(:last-child) {
+        border-bottom: solid 1.5px rgba(21, 19, 19, 0.05);
+      }
+
+      &__title {
+        display: block;
+        font-weight: bold;
+        margin-bottom: 10px;
+      }
+
+      &__content {
+        color: $dark-gray;
+      }
+    }
+  }
+
+  &__button {
+    padding: 20px 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
