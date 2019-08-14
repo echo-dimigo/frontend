35f0a6a381b66e048e88f0eef413b3267592e87d
uhmtoto <uhmtotow@gmail.com>
2019-08-14 17:56:23 +0900

    feat: echoos big input component
    아직 고칠 게 많음

---
+++ src/echoos/components/EchoosBigInput.vue
@@ -0,0 +1,60 @@
+<script>
+export default {
+  name: 'EchoosBigInput',
+  props: {
+    placeholder: {
+      type: String,
+      default: null
+    },
+    value: {
+      type: String,
+      default: null
+    }
+  },
+
+  data () {
+    return {
+      inputValue: null
+    }
+  },
+
+  watch: {
+    value (v) {
+      this.inputValue = v
+    },
+    inputValue (v) {
+      this.$emit('input', v)
+    }
+  },
+
+  methods: {
+    emitEnter () {
+      this.$emit('enter')
+    }
+  }
+}
+</script>
+
+<template>
+  <textarea
+    class="input"
+    v-model="inputValue"
+    :placeholder="placeholder"
+    @keyup.enter="emitEnter"
+  />
+</template>
+
+<style lang="scss" scoped>
+@import '../../scss/colors.scss';
+
+.input {
+  border: 0;
+  border-radius: 15px;
+
+  background-color: $gray;
+
+  padding: 1rem;
+
+  outline: none;
+}
+</style>
--- src/echoos/components/index.js
+++ src/echoos/components/index.js
@@ -1,2 +1,3 @@
 export { default as EchoosInput } from './EchoosInput.vue'
+export { default as EchoosBigInput } from './EchoosBigInput.vue'
 export { default as EchoosButton } from './EchoosButton.vue'
