e42eb320687dd78d7205ddc711344e74e661244b
uhmtoto <uhmtotow@gmail.com>
2019-08-14 13:14:28 +0900

    feat: navigation component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,90 @@
 <script>
 import EchoHeader from '@/components/EchoHeader.vue'
+import SwaggyProfile from '@/components/SwaggyProfile.vue'
+import SwaggyNavigation from '@/components/SwaggyNavigation.vue'
+import SwaggyNotification from '@/components/SwaggyNotification.vue'
 
 export default {
   name: 'App',
-  components: { EchoHeader }
+  components: { EchoHeader,
+    SwaggyProfile,
+    SwaggyNavigation,
+    SwaggyNotification }
 }
 </script>
 
 <template>
-  <div id="app">
+  <div id="app" class="app">
     <echo-header />
-    <router-view />
+    <div class="app__container">
+      <div class="app__menu">
+        <swaggy-profile
+          class="app__menu__profile"
+        />
+        <swaggy-navigation
+          class="app__menu__navigation"
+        />
+      </div>
+      <router-view
+        class="app__content"
+      />
+      <swaggy-notification
+        class="app__notification"
+      />
+    </div>
   </div>
 </template>
 
 <style lang="scss">
 @import './scss/normalize.css';
+@import './scss/colors.scss';
 @import url(//spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css);
 
 * {
   font-family: 'Spoqa Han Sans', 'Sans-serif';
+}
+
+.app {
+  min-height: 100vh;
+  background-color: $gray;
+
+  &__container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 20px;
+  }
+
+  &__menu {
+    width: 220px;
+
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+
+    &__profile,
+    &__navigation {
+      margin-bottom: 15px;
+    }
+  }
+
+  &__content {
+    width: 60%;
+
+    @media (max-width: 900px) {
+      width: 100%;
+      order: 1;
+    }
+  }
+
+  &__notification {
+    width: 250px;
+    height: 100%;
+    margin-bottom: 15px;
+
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+  }
 }
 </style>
---
+++ src/components/SwaggyNavigation.vue
@@ -0,0 +1,135 @@
+<script>
+export default {
+  name: 'SwaggyNavigation',
+
+  methods: {
+    push (val) {
+      this.current = val
+      this.$router.push(this.menus[val].to)
+    }
+  },
+
+  data () {
+    return {
+      current: 0,
+      menus: [
+        {
+          title: '구독 페이지 전체',
+          notification: 2,
+          to: ''
+        },
+        {
+          title: '페이지 전체',
+          notification: 0,
+          to: ''
+        },
+        {
+          title: '클립한 글',
+          notification: 0,
+          to: ''
+        },
+        {
+          title: 'JnJ 일반 동아리',
+          notification: 10,
+          to: ''
+        },
+        {
+          title: 'IT 학생회',
+          notification: 4,
+          to: ''
+        },
+        {
+          title: '대나무숲',
+          notification: 2,
+          to: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <nav class="nav">
+    <div
+      :key="`item-${i}`"
+      v-for="(item, i) in menus"
+      :class="{
+        'nav__item': true,
+        'nav__item-current': i === current
+      }"
+      @click="push(i)"
+    >
+      <div
+        v-if="item.notification"
+        :class="{
+          'nav__item__badge': true,
+          'nav__item__badge-current': i === current
+        }"
+      >
+        <span class="nav__item__badge__text">
+          {{ item.notification }}
+        </span>
+      </div>
+      <span class="nav__item__title">
+        {{ item.title }}
+      </span>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+@import '../scss/colors.scss';
+
+.nav {
+  background-color: $white;
+  padding: 1em 0;
+
+  border-radius: 6px;
+  border: solid 1.5px rgba(21, 19, 19, 0.05);
+
+  &__item {
+    padding: 10px 0 10px 10px;
+    color: $dark-gray;
+    width: 85%;
+
+    &-current {
+      background-color: $brand;
+
+      border-top-right-radius: 25px;
+      border-bottom-right-radius: 25px;
+
+      color: $white;
+    }
+
+    &__title {
+      user-select: none;
+    }
+
+    &__badge {
+      height: 1.5rem;
+      width: 1.5rem;
+      display: inline-block;
+      border-radius: 50%;
+      text-align: center;
+      vertical-align: middle;
+      margin-right: 5px;
+      background-color: $banana;
+
+      &-current {
+        background-color: $white;
+        opacity: 1;
+      }
+
+      &__text {
+        color: $dark-gray;
+        font-size: 0.8rem;
+
+        &-current {
+          color: $brand;
+        }
+      }
+    }
+  }
+}
+</style>
