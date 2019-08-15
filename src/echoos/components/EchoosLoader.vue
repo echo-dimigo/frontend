14394ba1043f23c31a5d42b3a06c4ad957cdad94
uhmtoto <uhmtotow@gmail.com>
2019-08-15 10:46:36 +0900

    feat: echoos loader component

---
+++ src/echoos/components/EchoosLoader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  name: 'EchoosLoader'
+}
+</script>
+
+<template>
+  <div class="loader" />
+</template>
+
+<style lang="scss" scoped>
+@import '../../scss/colors.scss';
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.loader {
+  border: 5px solid $white;
+  border-top: 5px solid $brand;
+  border-radius: 50%;
+  width: 5rem;
+  height: 5rem;
+  animation: spin 1s linear infinite;
+}
+</style>
--- src/echoos/components/index.js
+++ src/echoos/components/index.js
@@ -1,3 +1,4 @@
 export { default as EchoosInput } from './EchoosInput.vue'
 export { default as EchoosBigInput } from './EchoosBigInput.vue'
 export { default as EchoosButton } from './EchoosButton.vue'
+export { default as EchoosLoader } from './EchoosLoader.vue'
