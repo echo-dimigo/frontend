22a9618e0c782df034cc7f7a961ef7f26392258e
uhmtoto <uhmtotow@gmail.com>
2020-01-08 18:34:51 +0900

    feat: show toast when request/cancle ingang room

--- src/api/dimigoin/ingang.js
+++ src/api/dimigoin/ingang.js
@@ -7,8 +7,9 @@ export default {
     let { ingangs } = await deliverier(() => axios.get('/ingang'))
     return ingangs.map(Ingang)
   },
-  async requestIngang (idx) {
-    await deliverier(() => axios.post(`/ingang/${idx}`), {
+  async requestIngang (ingang) {
+    await deliverier(() => axios.post(`/ingang/${ingang.idx}`), {
+      200: `오늘의 ${ingang.time}타임 인강실을 신청했습니다.`,
       403: '인강실 티켓이 없습니다.',
       404: '해당 인강실이 존재하지 않습니다.',
       405: '신청 기간이 아닙니다.',
@@ -16,8 +17,9 @@ export default {
       409: '잔여 좌석이 없습니다.'
     })
   },
-  async cancelIngang (idx) {
-    await deliverier(() => axios.delete(`/ingang/${idx}`), {
+  async cancelIngang (ingang) {
+    await deliverier(() => axios.delete(`/ingang/${ingang.idx}`), {
+      200: `오늘의 ${ingang.time}타임 인강실을 취소했습니다.`,
       404: '해당 인강실이 존재하지 않습니다.',
       405: '취소 기한이 지났습니다.'
     })
