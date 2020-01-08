36521b6394a37f6d93cf49034062743a64cb2c9e
uhmtoto <uhmtotow@gmail.com>
2020-01-08 18:24:08 +0900

    refactor: use deliverier module in fetching meals

--- src/api/dimigoin/meal.js
+++ src/api/dimigoin/meal.js
@@ -1,10 +1,11 @@
 import axios from 'axios'
+import deliverier from '@/api/deliverier'
 import { format } from 'date-fns'
 
 export default {
   async getTodayMeal () {
     const today = format(new Date(), 'YYYYMMDD')
-    const { data: meal } = await axios.get(`/dimibobs/${today}`)
+    const meal = await deliverier(() => axios.get(`/dimibobs/${today}`))
     return meal
   }
 }
