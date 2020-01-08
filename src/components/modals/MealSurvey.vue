ee85369262149afbabeacc1093e756b4abd52189
uhmtoto <uhmtotow@gmail.com>
2020-01-08 21:49:36 +0900

    feat: initialize meal survey modal component

---
+++ src/components/modals/MealSurvey.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'MealSurvey',
+
+  props: {
+    mealCode: {
+      type: String,
+      required: true
+    }
+  },
+
+  data () {
+    return {
+      form: {
+        code: this.mealCode,
+        rating: 0,
+        message: 0
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <h3>급식 평가하기</h3>
+</template>
