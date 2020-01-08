484d6db95fb9a810f93535dd5abf668bdb059517
uhmtoto <uhmtotow@gmail.com>
2020-01-08 13:32:42 +0900

    feat: deliverier module to notify error message

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,24 +8,18 @@ export const configAxios = () => {
   axios.interceptors.response.use(response => {
     return response
   }, async error => {
-    try {
-      if (error.response.status === 401) {
-        try {
-          await store.dispatch('refreshAccessToken', localStorage.refreshToken)
-          error.config.headers = {
-            Authorization: `Bearer ${store.getters.accessToken}`
-          }
-          return axios.request(error.config)
-        } catch (e) {
-          Vue.$toast.error('리프레시 토큰이 만료되어 재로그인이 필요합니다.')
+    if (error.response.status === 401) { // token refresh
+      try {
+        await store.dispatch('refreshAccessToken', localStorage.refreshToken)
+        error.config.headers = {
+          Authorization: `Bearer ${store.getters.accessToken}`
         }
-      } else if (error.response.status !== 404) {
-        Vue.$toast.error('작업을 수행하던 중 에러가 발생했습니다.')
-        return Promise.reject(error)
+        return axios.request(error.config)
+      } catch (_error) {
+        Vue.$toast.error('리프레시 토큰이 만료되어 재로그인이 필요합니다.')
       }
-    } catch (e) {
-      Vue.$toast.error('네트워크 연결을 확인해 주세요.')
-      return Promise.reject(error)
+    } else {
+      throw error
     }
   })
 }
---
+++ src/api/deliverier.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+
+export default async (action, handlers = {}) => {
+  try {
+    return await action()
+  } catch (error) {
+    const status = error.response.status
+
+    const message = Object.keys(handlers).includes(String(status))
+      ? handlers[status] : '작업을 수행하던 중 에러가 발생했습니다.'
+
+    Vue.$toast.error(message)
+
+    return Promise.reject(error)
+  }
+}
