7c08c6ec37c665175cd20822231996dc30ce64b2
uhmtoto <uhmtotow@gmail.com>
2020-01-08 18:40:48 +0900

    fix: not showing toast issue when request was unsuccessful

--- src/api/deliverier.js
+++ src/api/deliverier.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 
 export default async (action, handlers = {}) => {
-  let status; let message = ''
-
   try {
     const response = await action()
-
-    status = response.status
+    const status = response.status
     if (Object.keys(handlers).includes(String(status))) {
-      message = handlers[status]
+      Vue.$toast.success(handlers[status])
     }
-
-    if (message) Vue.$toast.success(message)
 
     return response.data
   } catch (error) {
-    if (message) Vue.$toast.error(message)
-
+    const status = error.response.status
+    if (Object.keys(handlers).includes(String(status))) {
+      Vue.$toast.error(handlers[status])
+    }
     return Promise.reject(error)
   }
 }
