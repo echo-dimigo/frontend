0fa4c3b2e0fb28ab411ac2f3ec870c29af6639ee
uhmtoto <uhmtotow@gmail.com>
2020-01-09 23:21:57 +0900

    fix: add missing dollar sign

--- src/api/deliverier.js
+++ src/api/deliverier.js
@@ -12,7 +12,7 @@ export default async (action, handlers = {}) => {
   } catch (error) {
     const status = error.response.status
     if (Object.keys(handlers).includes(String(status))) {
-      Vue.swal('이런!', handlers[status], 'error')
+      Vue.$swal('이런!', handlers[status], 'error')
     }
     return Promise.reject(error)
   }
