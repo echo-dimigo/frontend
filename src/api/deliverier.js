c4d76af98bff9cc385cdfd0a0dc4a7beb7474560
uhmtoto <uhmtotow@gmail.com>
2020-01-11 22:42:32 +0900

    fix: toast in deliverier module

--- src/api/deliverier.js
+++ src/api/deliverier.js
@@ -1,18 +1,28 @@
-import Vue from 'vue'
+import Swal from 'sweetalert2'
+
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  confirmButtonColor: '#5fae9f',
+  confirmButtonText: '확인',
+  cancelButtonColor: '#c5593b',
+  cancelButtonText: '취소',
+  animation: false
+})
 
 export default async (action, handlers = {}) => {
   try {
     const response = await action()
     const status = response.status
     if (Object.keys(handlers).includes(String(status))) {
-      Vue.$swal('성공!', handlers[status], 'success')
+      Toast.fire('성공!', handlers[status], 'success')
     }
 
     return response.data
   } catch (error) {
     const status = error.response.status
     if (Object.keys(handlers).includes(String(status))) {
-      Vue.$swal('이런!', handlers[status], 'error')
+      Toast.fire('이런!', handlers[status], 'error')
     }
     return Promise.reject(error)
   }
